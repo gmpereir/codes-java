b918c2f6d0418a68eeadaf80e7c23578250280fe
gmpereir@hotmail.com <gmper@gmpereir>
2019-08-19 15:20:20 -0300

    Updating file "Vector.java"

--- studyingJava/src/br/com/gmpereir/javaexercises/Vector.java
+++ studyingJava/src/br/com/gmpereir/javaexercises/Vector.java
@@ -1,14 +1,31 @@
 package br.com.gmpereir.javaexercises;
 
+import java.util.Scanner;
+
 /**
  * @author Gustavo Pereira
  */
 public class Vector {
-    
+
     public static void main(String[] args) {
-        
-        
-        
+
+        Scanner input = new Scanner(System.in);
+
+        int numStudents = 5;
+        float[] scoreStudents = new float[numStudents];
+
+        for (int i = 0; i < numStudents; ++i) {
+
+            System.out.println("Enter the score of " + (i + 1) + "º student.");
+            scoreStudents[i] = input.nextFloat();
+
+        }
+
+        for (int i = 0; i < numStudents; ++i) {
+
+            System.out.println("Score of " + (i + 1) + "º student: " + scoreStudents[i]);
+        }
+
     }
-    
+
 }
