f315661b55d68a8bd6820a78a68f9b1ebd790a8b
gmpereir@hotmail.com <gmper@gmpereir>
2019-08-19 15:52:40 -0300

    Updating file "BubbleSort.java"

--- studyingJava/src/br/com/gmpereir/javaexercises/BubbleSort.java
+++ studyingJava/src/br/com/gmpereir/javaexercises/BubbleSort.java
@@ -13,14 +13,20 @@ public class BubbleSort {
 
         for (int i = 0; i < vector.length; ++i) {
 
+            control = true;
             for (int j = 0; j < (vector.length - 1); ++j) {
 
                 if (vector[j] > vector[j + 1]) {
                     aux = vector[j];
                     vector[j] = vector[j + 1];
                     vector[j + 1] = aux;
+                    control = false;
                 }
 
+            }
+            if (control) {
+                break;
+                
             }
 
         }
